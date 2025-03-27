32aacb658daff036fd23b438039c3eb826bfe896
zambodev1 <zambodev6@gmail.com>
2025-03-27 20:54:49 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'zambo dev', // Your name
-  ownerNumber: '8068300775', //Your number
+  botOwner: 'zambo bot', // Your name
+  ownerNumber: '+23408068300775', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
